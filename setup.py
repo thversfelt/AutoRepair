6e02c7e8acc8ab2c848604572e28319d853065ec
Thijs Versfelt <thversfelt@hotmail.com>
2023-03-08 16:02:54 +0100

    Removed version dependency for autotest

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='autorepair',
-    version='0.1.2',
+    version='0.1.3',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'pymoo==0.6.0.1',
-        'autotest==0.1.1'
+        'autotest'
     ],
     dependency_links=[
         'git+https://github.com/thversfelt/AutoTest.git#egg=autotest'
