ee362a15af7bedda2473d56ec683ea409d351178
Thijs Versfelt <thversfelt@hotmail.com>
2023-03-08 15:54:00 +0100

    Updated autotest dependency version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='autorepair',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'pymoo==0.6.0.1',
-        'autotest'
+        'autotest=0.1.1'
     ],
     dependency_links=[
         'git+https://github.com/thversfelt/AutoTest.git#egg=autotest'
