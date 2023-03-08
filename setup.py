25842b169d132e69a78528d1ceb34c23f1c1c73d
Thijs Versfelt <thversfelt@hotmail.com>
2023-03-08 15:55:59 +0100

    Fixed a syntax error in setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='autorepair',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'pymoo==0.6.0.1',
-        'autotest=0.1.1'
+        'autotest==0.1.1'
     ],
     dependency_links=[
         'git+https://github.com/thversfelt/AutoTest.git#egg=autotest'
